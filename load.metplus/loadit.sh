3321cac50cf5314832feff361e12960efbcb9a74
YingLin-NOAA <ying.lin@noaa.gov>
2019-10-16 17:27:52 +0000

    update

--- load.metplus/loadit.sh
+++ load.metplus/loadit.sh
@@ -2,11 +2,30 @@
 
 # Load PCPRTMA vs. PCPRTMA2p8 stats:
 SCRIPTS=/gpfs/dell2/emc/verification/noscrub/Ying.Lin/awsmv
-STATS=/gpfs/dell2/ptmp/Ying.Lin/awsload
+
+STATSARCH=/gpfs/dell2/emc/verification/noscrub/Ying.Lin/metplus.stats
+STATSWRK=/gpfs/dell2/ptmp/Ying.Lin/awsload.metplus
+
+if [ -d $STATSWRK/stats.dir ]; then
+  rm -rf $STATSWRK/stats.dir/*
+else
+  mkdir -p $STATSWRK/stats.dir
+fi
+
+cd $STATSWRK/stats.dir
+
+for dd in 01 02 03 04 05 06
+do
+  cp -p $STATSARCH/201910${dd}/*10${dd}_21*.stat . 
+done
+
+cp -p $STATSARCH/20191007/*.stat . 
+cp -p $STATSARCH/20191008/*.stat . 
+
 # subdir under $STATS defined in the xml script: 
 #     <folder_tmpl>/base_dir/{type}</folder_tmpl>
 #              ....
-#       <val>met.stats</val>
+#       <val>stats.dir</val>
 
-$SCRIPTS/mv_load_to_aws.sh ying.lin $STATS $SCRIPTS/load.metplus/load_metplus_add.xml
+$SCRIPTS/mv_load_to_aws.sh ying.lin $STATSWRK $SCRIPTS/load.metplus/load_metplus_add.xml
 
