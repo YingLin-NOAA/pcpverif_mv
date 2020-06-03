07fc8df4d11ae1d2f93ab4fa5433c794d75c9f80
Ying Lin <ying.lin@noaa.gov>
2020-06-03 21:28:37 +0000

    update

--- load.metplus/cp_stats.sh
+++ load.metplus/cp_stats.sh
@@ -1,11 +1,13 @@
 #!/bin/sh
 # copy over stats from $day1 to $day2 to a temp directory
 #
-day1=20190914
-day2=20200331
+day1=20191005
+day2=20200524
 
 STATSDIR=/gpfs/dell2/emc/verification/noscrub/Ying.Lin/metplus.stats
-tmpdir=/gpfs/dell2/stmp/Ying.Lin/met.stats.for.mars
+#tmpdir=/gpfs/dell2/stmp/Ying.Lin/met.stats.for.mars
+tmpdir=/gpfs/dell2/ptmp/Ying.Lin/awsload.metplus/stats.dir
+
 cd $STATSDIR
 for day in `ls -1 | grep 20`
 do
--- load/load_tmp.sh
+++ load/load_tmp.sh
@@ -1,7 +1,29 @@
 #!/bin/sh
+#BSUB -J VSDB_load
+#BSUB -P VERF-T2O
+#BSUB -n 1
+#BSUB -o /gpfs/dell2/ptmp/Ying.Lin/cron.out/vsdb_load.%J
+#BSUB -e /gpfs/dell2/ptmp/Ying.Lin/cron.out/vsdb_load.%J
+#BSUB -W 5:58
+#BSUB -q "dev_shared"
+#BSUB -R "rusage[mem=1500]"
+#BSUB -R affinity[core(1)]
+#BSUB -R "span[ptile=1]"
 set -x
-AWSMV=/gpfs/dell2/emc/verification/noscrub/Ying.Lin/awsmv
-STATS=/gpfs/dell2/ptmp/Ying.Lin/awsload
-$AWSMV/mv_load_to_aws.sh ying.lin $STATS $AWSMV/load/load_pcp_add.xml
+
+# Load prod and dev vsdbs for $daym9
+
+# also can run interactively with an argument (day0)
+
+prdvsdbdir=/gpfs/dell1/nco/ops/com/verf/prod/vsdb/precip
+devvsdbdir=/gpfs/dell2/ptmp/Ying.Lin/verf.dat/vsdb
+
+PTMP=/gpfs/dell2/ptmp/Ying.Lin/awsload
+dirvsdb=$PTMP/vsdb
+  
+# Now load to AWS MV:
+# $AWSMV defined in .bashrc.
+STATS=$PTMP
+$AWSMV/mv_load_to_aws.sh ying.lin $STATS $AWSMV/load/load_pcp_vsdb.xml
 
 exit
